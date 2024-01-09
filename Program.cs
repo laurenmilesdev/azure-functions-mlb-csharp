334e8f99faaab0d85c79086723e56814868532ce
Lauren <laurenmiles.dev@gmail.com>
2024-01-08 20:25:43 -0500

    Added http client for api

--- Program.cs
+++ Program.cs
@@ -1,14 +1,33 @@
-using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
-    .ConfigureServices(services =>
+namespace AzureFunctionsMlbCSharp
+{
+    public class Program
     {
-        services.AddApplicationInsightsTelemetryWorkerService();
-        services.ConfigureFunctionsApplicationInsights();
-    })
-    .Build();
+        public static void Main()
+        {
+            var builder = new HostBuilder()
+                .ConfigureFunctionsWorkerDefaults()
+                .ConfigureServices(ConfigureServices);
 
-host.Run();
+            var host = builder.Build();
+
+            host.Run();
+        }
+
+        private static string rapidApiMlbBaseUrl = Environment.GetEnvironmentVariable("X_RAPIDAPI_HOST") ?? "";
+
+        static void ConfigureServices(HostBuilderContext builder, IServiceCollection services)
+        {
+            services
+                .AddHttpClient("mlb", (provider, client) =>
+                {
+                    client.BaseAddress = new Uri($"https://{rapidApiMlbBaseUrl}");
+                    client.DefaultRequestHeaders.Add("Accept", "application/json");
+                    client.DefaultRequestHeaders.Add("X-RapidAPI-Key", Environment.GetEnvironmentVariable("X_RAPIDAPI_KEY"));
+                    client.DefaultRequestHeaders.Add("X-RapidAPI-Host", rapidApiMlbBaseUrl);
+                });
+        }
+    }
+}
