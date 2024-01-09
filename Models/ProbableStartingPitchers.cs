4226a22bb64f016026fa011a8fb42484770810e3
Lauren <laurenmiles.dev@gmail.com>
2024-01-08 20:33:10 -0500

    Created models for data

---
+++ Models/ProbableStartingPitchers.cs
@@ -0,0 +1,8 @@
+namespace AzureFunctionsMlbCSharp
+{
+    public class ProbableStartingPitchers
+    {
+        public string? Away { get; set; }
+        public string? Home { get; set; }
+    }
+}
