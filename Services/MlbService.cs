6a183fc436cb45938c14a7313c418c79250a2a81
Lauren <laurenmiles.dev@gmail.com>
2024-01-10 09:17:44 -0500

    Updated param name

--- Services/MlbService.cs
+++ Services/MlbService.cs
@@ -11,11 +11,11 @@ namespace AzureFunctionsMlbCSharp
             _clientFactory = clientFactory;
         }
 
-        async Task<TeamSchedule?> IMlbService.GetTeamSchedule(string teamAbv, string? season)
+        async Task<TeamSchedule?> IMlbService.GetTeamSchedule(string team, string? season)
         {
             var client = _clientFactory.CreateClient("mlb");
 
-            HttpResponseMessage response = await client.GetAsync($"/getMLBTeamSchedule?teamAbv={teamAbv.ToUpper()}&season={season ?? DateTime.Now.Year.ToString()}");
+            HttpResponseMessage response = await client.GetAsync($"/getMLBTeamSchedule?teamAbv={team.ToUpper()}&season={season ?? DateTime.Now.Year.ToString()}");
 
             return response.IsSuccessStatusCode ? (await response.Content.ReadFromJsonAsync<TeamScheduleResponse>())?.Schedule : null;
         }
